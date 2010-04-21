c2aedc85cd3738f6b4e44c28a6845df7f30da2cf
Justin Davis <jrcd83@gmail.com>
2010-04-21 00:11:19 -0700

    Got to a working state to start having fun.  Made silly docs.

--- lib/Peu.pm
+++ lib/Peu.pm
@@ -3,96 +3,151 @@ package Peu;
 use warnings;
 use strict;
 
+our $VERSION = '0.01';
+
+use Router::Simple;
+use Peu::Request;
+use English qw(-no_match_vars);
+
+sub import
+{
+    my $caller_pkg = caller(0);
+    my $router     = Router::Simple->new();
+
+    # These get added to later and are checked when a match occurs.
+    my @before_filters;
+    my @error_handlers;
+
+    my $response_maker = sub {
+        my ($usercode_ref) = @_;
+
+        sub {
+            my $match_ref = shift;
+            $_->() foreach @{ delete $match_ref->{ '_befores' } };
+
+            # Store route parameters...
+            *{ "${caller_pkg}::Prm" } = $match_ref;
+
+            # Catch errors and use error handlers later...
+            my @response = eval { $usercode_ref->() };
+            return [ 500, 'text/plain', '500 Internal Server Error' ]
+                if $EVAL_ERROR;
+            return Peu::Response->new( @response );
+        }
+    };
+
+    # Sort of copied from Router::Simple::Cookbook
+    my $route_maker = sub {
+        no strict 'refs';
+        my ($http_method) = @_;
+        *{ "${caller_pkg}::${http_method}" } = sub {
+            my $route    = shift;
+            my $code_ref = shift;
+            
+            $router->connect( $route,
+                              { '_befores' => [ @before_filters ],
+                                '_errors'  => [ @error_handlers ],
+                                '_code'    => $response_maker->($code_ref),
+                               },
+                              ( $http_method eq 'any' ? ()
+                                : { method => uc $http_method } ),
+                             );
+        }
+    };
+
+    {
+        no strict 'refs';
+
+        my $anon_scalar;
+        *{ "${caller_pkg}::Req" } = \$anon_scalar;
+        *{ "${caller_pkg}::Rtr" } = \$router;
+        *{ "${caller_pkg}::Prm" } = {};
+
+        *{ "${caller_pkg}::to_app" } = sub {
+            sub {
+                my $req_ref = shift;
+                *{ "${caller_pkg}::Req" } = Peu::Req->new( $req_ref );
+
+                my $match_ref = $router->match( $req_ref )
+                    or return [ 404,
+                                [ 'ContentType' => 'text/html' ],
+                                [ '404 Not found' ],
+                               ];
+
+                ( delete $match_ref->{ '_code' } )->();
+            }
+        };
+
+    }
+
+    $route_maker->( $_ ) foreach qw/ any get post delete update /;
+}
+
+1; # End of Peu
+
+__END__
+
 =head1 NAME
 
-Peu - The great new Peu!
+Peu - Un peu web "framework".
 
 =head1 VERSION
 
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
-
+0.01
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+  use Peu;
 
-Perhaps a little code snippet.
+  any "/api/{version}/{arg}" => {
+    "Hello, I'm Peu!  You want version $Prm{version}?  $Prm{arg} you!"
+  }
 
-    use Peu;
+=head1 DESCRIPTION
 
-    my $foo = Peu->new();
-    ...
+This is a micro framework that is even smaller than micro... it's peu!
+Peu means "little" in French.  It's another DSL framework with less
+features in about 100 lines of code.
 
-=head1 EXPORT
+=head1 WHY
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+I sort of started to use Plack to write a B<REALLY> simple application.
+However Plack's docs kept saying "don't do that! we're for frameworks
+only, stupid!".  So I made a super simple one that let me use
+Route::Simple and Plack::Request.  Neato!
 
-=head1 SUBROUTINES/METHODS
+=head1 HOW
 
-=head2 function1
+Well it's sort of like another Sinatra clone, but more like one that
+is deformed and missing important bits.
 
-=cut
+=head2 any / get / post / delete / update
 
-sub function1 {
-}
+TODO
 
-=head2 function2
+=head2 Package Variables
 
-=cut
-
-sub function2 {
-}
-
-=head1 AUTHOR
-
-Justin Davis, C<< <juster at cpan.org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-peu at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Peu>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Peu
-
-
-You can also look for information at:
+A few package variables are created into the importer's namespace.
+This is so they can be used inside any of the router response blocks.
 
 =over 4
 
-=item * RT: CPAN's request tracker
+=item $Req - A Peu::Req (same as L<Plack::Request>) object.
 
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Peu>
+=item %Prm - Route parameters, if you have specified any.
 
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Peu>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Peu>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Peu/>
+=item $Rtr - A L<Router::Simple> object that's matching things.
 
 =back
 
+=head1 HUH
 
-=head1 ACKNOWLEDGEMENTS
+I will probably add more feature if I need them.  Or y'know just start
+using L<Dancer>.
 
+=head1 SEE ALSO
+
+These are also the dependencies: L<Router::Simple> and L<Plack>.
 
 =head1 LICENSE AND COPYRIGHT
 
@@ -104,7 +159,3 @@ by the Free Software Foundation; or the Artistic License.
 
 See http://dev.perl.org/licenses/ for more information.
 
-
-=cut
-
-1; # End of Peu
---
+++ lib/Peu/Request.pm
@@ -0,0 +1,11 @@
+package Peu::Request;
+
+use warnings;
+use strict;
+use base qw(Plack::Request);
+
+
+
+1;
+
+__END__
