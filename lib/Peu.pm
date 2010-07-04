6b38867b8fe8fada22580b8dd4b18d0e64dee0b3
Justin Davis <jrcd83@gmail.com>
2010-07-04 12:14:23 -0700

    Remove extraneous "view" logic.  Great reorganization.
    Started using a naming scheme for all my closures and their variables.
    Removed _get_data_templates.  Preparing to create extensions.

--- lib/Peu.pm
+++ lib/Peu.pm
@@ -8,83 +8,72 @@ our $VERSION = '0.01';
 use Router::Simple;
 use Peu::Req;
 use Peu::Res;
-use Peu::Ext::Fu;
 
 use English    qw(-no_match_vars);
 use Carp       qw();
 
 $Carp::CarpInternal{ __PACKAGE__ } = 1;
 
+# This defines global attribute handlers.  They affect every Peu web app.
+my %USER_ATTRIBS;
+sub ATTRIB
+{
+    my ($name, $handler_ref) = @_;
+
+    Carp::croak( 'Invalid arguments to ATTRIB' )
+        if ( ! $name || ref $name || ref $handler_ref ne 'CODE' );
+
+    $USER_ATTRIBS{ $name } = $handler_ref;
+}
+
 #---HELPER FUNCTION---
 # Pass it a reference to the attributes list.  Removes recognized
 # attributes from the list and returns the value to use for the
 # http_method argument to $router->connect
-sub _extract_req_attribs
+sub _extract_route_attribs
 {
     my ($attribs_ref) = @_;
 
     my ( @unknown_attribs, %results );
 
+    my $user_attrib_match =
+        sprintf '\A(%s)(?:[(]([^)]+)[)]\z)?', join q{|}, keys %USER_ATTRIBS;
+    my $user_data = $results{'data'} = {};
+
     ATTRIB_LOOP:
     while ( my $attrib = shift @$attribs_ref ) {
         unless ( $attrib =~ / \A (ANY|GET|POST|UPDATE|DEL|DELETE|VIEW)
                               [(] ([^)]+) [)] \z /xms ) {
-            push @unknown_attribs, $attrib;
-            next ATTRIB_LOOP;
-        }
 
-        if ( $1 eq 'VIEW' ) {
-            Carp::croak( 'You can only have one :VIEW attribute' )
-                if $results{ 'viewargs' };
-            $results{'viewargs'} = [ split /\s*,\s*/, $2 ];
+            # Call our custom attribute handler if our HTTP request type
+            # attributes did not match...
+            if ( $attrib =~ /$user_attrib_match/ ) {
+                my $name = $1;
+                eval { $USER_ATTRIBS{ $name }->( $2, $user_data ) };
+                next ATTRIB_LOOP unless $@;
+                Carp::croak( "Attribute handler for $name failed: $@" );
+            }
+
+            push @unknown_attribs, $attrib;
             next ATTRIB_LOOP;
         }
 
         Carp::croak( 'You can only have one HTTP-method attribute' )
             if $results{ 'method' };
+
         $results{'method'} = $1;
-        $results{'route'}    = $2;
+        $results{'route'}  = $2;
     }
 
     @$attribs_ref = @unknown_attribs;
-    $results{'viewargs'} ||= [];
 
     return %results;
-}
-
-#---HELPER FUNCTION---
-sub _get_data_templates
-{
-    my ($package) = @_;
-
-    my $datafh = do { no strict 'refs'; *{ $package . '::DATA' } };
-
-    return () if eof $datafh;
-
-    my ( %result, $name, $text );
-
-    LINE_LOOP:
-    while ( my $line = <$datafh> ) {
-        unless ( $line =~ / ^ \s* __([\w.-]+)__ \s* $ /xms ) {
-            $text .= $line;
-            next LINE_LOOP;
-        }
-        if ( $name ) {
-            $text =~ s/\n{2,}\z/\n/; # remove newlines between entries
-            $result{ $name } = $text;
-        }
-        $name = $1;
-        $text = q{};
-    }
-
-    $result{ $name } = $text if $text;
-    return %result;
 }
 
 sub import
 {
     my $caller_pkg = caller 0;
-    my $router     = Router::Simple->new();
+    my $V_router   = Router::Simple->new();
 
 #     local $Carp::Internal{ (__PACKAGE__) } = 1;
 
@@ -101,10 +90,130 @@ sub import
     };
 
     ######################################################################
-    # CONFIGURATION OPTIONS
+    # CORE WEBAPP LOGIC
+    #---------------------------------------------------------------------
+
+    my $C_execute_route; # This is defined later on...
+    my ($V_response_obj, $V_request_obj, $V_route_params);
+
+    # DEFAULT handlers match when nothing else does...
+    my $default_handler = sub { [ 404,
+                                  [ 'Content-Type' => 'text/html' ],
+                                  [ '<h1>404 Not found</h1>' ],
+                                 ];
+                            };
+
+    # Declare package variables in the caller package..
+    $liason->( 'Res' => \$V_response_obj ); 
+    $liason->( 'Req' => \$V_request_obj  );
+    $liason->( 'Prm' => $V_route_params );
+
+    # my ( %empty_hash, $empty_scalar );
+    # $liason->( 'Res' => \$empty_scalar ); # response object
+    # $liason->( 'Req' => \$empty_scalar ); # request object
+    # $liason->( 'Prm' => \%empty_hash   ); # route parameters
+
+    my $psgi_app = sub {
+        my $req_ref = shift;
+
+        # If PATH_INFO is blank we can never match it to a route...
+        $req_ref->{PATH_INFO} ||= q{/};
+
+        # Create a default response object...
+        $V_response_obj = Peu::Res->new();
+        $V_response_obj->status( 200 );
+        $V_response_obj->content_type( 'text/html' );
+
+        # Create a request object for this new request...
+        $V_request_obj = Peu::Req->new( $req_ref );
+
+        my $match_ref = $V_router->match( $req_ref );
+        $match_ref ||= $default_handler;
+
+        $C_execute_route->( $match_ref );
+    };
+
+    my $to_app = sub { $psgi_app };
+    $liason->( 'to_app' => $to_app );
+    $liason->( 'FIN'    => $to_app );
+
+    ######################################################################
+    # HTTP RESPONSES
+    #---------------------------------------------------------------------
+
+    # We can wrap this closure in order to add extra functionality...
+    $C_execute_route = sub {
+        my $route_data = shift;
+
+        # Store route parameters in the caller package.
+        # (Also keep a copy to share between closures)
+        # Do not copy internal use keys which begin with _
+        $V_route_params = +{ map  { ( $_ => $route_data->{$_} ) }
+                           grep { ! /^_/ } keys %$route_data };
+        $liason->( 'Prm' => $V_route_params );
+
+        # Catch errors and use error handlers later...
+        my $result = eval { $route_data->{'_code'}->() };
+        die if $EVAL_ERROR;
+        return $result;
+    };
+
+    my $EX_wrap = sub {
+        my $wrapper = shift;
+
+        my $old = $C_execute_route;
+        $C_execute_route = sub {
+            $wrapper->( $old, @_ );
+        };
+    };
+    $liason->( 'WRAP' => $EX_wrap );
+
+    # Treat non-reference data like response body text...
+    $EX_wrap->( sub {
+                    my $next = shift;
+                    my $result = $next->( @_ );
+
+                    return $result if ( ref $result );
+
+                    $V_response_obj->body( $result );
+                    return $V_response_obj->as_aref;
+                } );
+
+    # We use attributes for specifying routes...
+    # MODIFY_CODE_ATTRIBUTES is called when a sub is defined with attributes
+    $liason->( 'MODIFY_CODE_ATTRIBUTES' => sub {
+                   my ($pkg, $coderef, @attribs) = @_;
+                   my %resp = _extract_route_attribs( \@attribs );
+
+                   # Return any attributes we don't recognize...
+                   return @attribs if @attribs;
+
+                   $V_router->connect
+                       ( $resp{'route'},
+                         { %{$resp{'data'}}, '_code' => $coderef },
+                         ( $resp{'method'} eq 'ANY'
+                           ? () : { method => $resp{'method'} } ),
+                        );
+
+                   return qw//;
+               } );
+
+    ######################################################################
+    # MISCELLANIOUS KEYWORDS
+    #---------------------------------------------------------------------
+
+    # The "mid" keyword enables Plack middleware.
+    my $EX_mid = sub {
+        my $class_name = 'Plack::Middleware::' . ucfirst shift;
+        eval "require $class_name";
+        Carp::croak( "MID failed to load $class_name: $@" )
+            if $@;
+        $psgi_app = $class_name->wrap( $psgi_app, @_ );
+    };
+    $liason->( 'MID' => $EX_mid );
 
     my %config_keywords;
-    my $EX_set_config = sub {
+    my $EX_cfg = sub {
         my $name = shift;
 
         Carp::croak "$name is not a valid config key"
@@ -118,124 +227,7 @@ sub import
         undef $config_keywords{ $name };
         return;
     };
-    $liason->( 'CFG' => $EX_set_config );
-
-    ######################################################################
-    # HTTP RESPONSES
-
-    # This curries the coderef passed to the http method keyword.
-    my $resp_curry = sub {
-        my ($usercode_ref) = @_;
-
-        return sub {
-            my $match_ref = shift;
-
-            # Store route parameters in the caller package...
-            # Do not copy internal use keys which begin with _
-            my %params = map { ( $_ => $match_ref->{$_} ) }
-                grep { ! /^_/ } keys %$match_ref;
-            $liason->( 'Prm' => \%params );
-
-            # Catch errors and use error handlers later...
-            my $result = eval { $usercode_ref->() };
-            die if $EVAL_ERROR;
-
-            @VIEW_PARAMS = @{ $match_ref->{'_viewargs'} };
-
-            if ( @VIEW_PARAMS ) {
-                $result = $render_ref->( ref $result eq 'HASH'
-                                         ? $result
-                                         : \%params );
-            }
-
-            my $reftype = ref $result;
-
-            # Acknowledge raw PSGI responses...
-            return $result if $reftype eq 'ARRAY';
-
-            if ( $reftype eq 'HASH' ) {
-                Carp::croak 'Response needs a :VIEW attribute when '
-                    . 'returning hashrefs';
-            }
-
-            Carp::croak "Response result must be a scalar, aref, or hashref"
-                if $reftype || ! defined $result;
-            
-            my $res = ${ *{ $liason->( 'Res' ) }{SCALAR} };
-            $res->body( $result );
-            return $res->as_aref;
-        }
-    };
-
-    # We use attributes for specifying routes...
-    # MODIFY_CODE_ATTRIBUTES is called when a sub is defined with attributes
-    $liason->( 'MODIFY_CODE_ATTRIBUTES' => sub {
-                   my ($pkg, $coderef, @attribs) = @_;
-                   my %resp = _extract_req_attribs( \@attribs );
-
-                   # Return any attributes we don't recognize...
-                   return @attribs if @attribs;
-
-                   $router->connect
-                       ( $resp{'route'},
-                         { %{$resp{'data'}}, '_code' => $coderef },
-                         ( $resp{'method'} eq 'ANY'
-                           ? () : { method => $resp{'method'} } ),
-                        );
-
-                   return qw//;
-               } );
-
-    # DEFAULT handlers match when nothing else does...
-    my $default_handler = sub { [ 404,
-                                  [ 'Content-Type' => 'text/html' ],
-                                  [ '<h1>404 Not found</h1>' ],
-                                 ];
-                            };
-
-    # Declare package variables in the caller package..
-    my ( %empty_hash, $empty_scalar );
-    $liason->( 'Res' => \$empty_scalar ); # response object
-    $liason->( 'Req' => \$empty_scalar ); # request object
-    $liason->( 'Prm' => \%empty_hash   ); # route parameters
-
-    my $psgi_app = sub {
-        my $req_ref = shift;
-
-        # If PATH_INFO is blank we can never match it to a route...
-        $req_ref->{PATH_INFO} ||= q{/};
-
-        # Create a default response object...
-        my $response = Peu::Res->new();
-        $response->status( 200 );
-        $response->content_type( 'text/html' );
-
-        $liason->( 'Res' => \$response ); 
-        $liason->( 'Req' => \Peu::Req->new( $req_ref ) );
-        $liason->( 'Prm' => {} );
-
-        my $match_ref = $router->match( $req_ref );
-        $match_ref ||= $default_handler;
-
-        my $responder_ref = $match_ref->{ '_code' };
-        $responder_ref->( $match_ref );
-    };
-
-    my $to_app = sub { $psgi_app };
-    $liason->( 'to_app' => $to_app );
-    $liason->( 'FIN'    => $to_app );
-
-    # The "mid" keyword enables Plack middleware.
-    my $mid_keyword = sub {
-        my $name = ucfirst shift;
-
-        my $class_name = "Plack::Middleware::$name";
-        eval "require $class_name";
-        Carp::croak( "MID could not find a class called $class_name" )
-            if $@;
-        $psgi_app = $class_name->wrap( $psgi_app, @_ );
-    };
-    $liason->( 'MID' => $mid_keyword );
+    $liason->( 'CFG' => $EX_cfg );
 
     return;
 }
